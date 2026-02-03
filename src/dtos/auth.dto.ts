0d19926788216b3015fc009038ff13a07bba1f30
eaves0118 <tranhieudz2024@gmail.com>
2026-02-03 22:37:30 +0700

    update

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,11 @@
+export interface RegisterDTO {
+  email: string
+  username: string
+  password: string
+  displayName: string
+}
+
+export interface LoginDTO {
+  username: string
+  password: string
+}
